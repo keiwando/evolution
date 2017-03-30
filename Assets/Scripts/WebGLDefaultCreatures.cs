afedf2db70b21bca970109f0d600c034e2e12461
Keiwan Donyagard <keiwan@gmx.de>
2017-03-30 23:42:05 +0200

    Added custom WebGL save and load functionality & bugfixes

---
+++ Assets/Scripts/WebGLDefaultCreatures.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class WebGLDefaultCreatures  {
+
+	public static Dictionary<string, string> DefaultCreatures = new Dictionary<string, string> {
+
+		{"Hailer", 
+			"1%-5.379086%7.60694%0\n2%3.863863%7.651207%0\n3%10.3837%14.37072%0\n4%-3.772128%10.28738%0\n5%-6.625983%10.56094%0\n--%%--\n6%5%1\n7%1%4\n8%4%5\n9%1%2\n10%2%3\n--%%--\n11%10%9\n13%7%9\n"
+		}
+	};
+}
