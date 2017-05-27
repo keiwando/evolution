987f49d1c8c2e13373a7a7abdce73d26b1ca4567
Keiwan Donyagard <keiwan@gmx.de>
2017-05-27 23:48:08 +0200

    Implemented the option to save and load evolution simulation stages

---
+++ Assets/Scripts/ChromosomeInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public struct ChromosomeInfo
+{
+	public string chromosome {get; private set; }
+	public float fitness {get; private set; }
+
+	public ChromosomeInfo(string chromosome, float fitness) {
+		this.chromosome = chromosome;
+		this.fitness = fitness;
+	}
+
+	public override string ToString ()
+	{
+		return string.Format("{0}:{1}", chromosome, fitness.ToString());
+	}
+
+	public static ChromosomeInfo FromString(string str) {
+
+		var parts = str.Split(':');
+
+		return new ChromosomeInfo(parts[0], float.Parse(parts[1]));
+	}
+}
+
---
+++ Assets/Scripts/SimulationLoadDialog.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimulationLoadDialog : MonoBehaviour {
+
+	[SerializeField] private CreatureBuilder creatureBuilder;
+	[SerializeField] private Evolution evolution;
+
+	[SerializeField] private Dropdown dropdown;
+
+	[SerializeField] private GameObject bugFixEmpty;
+
+	private const string NO_SAVE_FILES = "You haven't saved any simulations yet";
+
+	private bool saveFilesExist = false;
+
+	// Use this for initialization
+	void Start () {
+		/*this.gameObject.SetActive(true);
+		this.gameObject.SetActive(false);
+		this.gameObject.SetActive(true);*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		/*if (bugFixEmpty.activeSelf) {
+
+			if (saveFilesExist) {
+				//dropdown.Show();
+			} else {
+				dropdown.Hide();
+			}
+		}*/
+	}
+
+	public void PromptDialog() {
+		//this.gameObject.SetActive(true);
+		bugFixEmpty.SetActive(true);
+		SetupDropDown();
+	}
+
+	public void OnCancelClicked() {
+		//this.gameObject.SetActive(false);
+		bugFixEmpty.SetActive(false);
+	}
+
+	public void OnLoadClicked() {
+
+		var filename = dropdown.options[dropdown.value].text;
+
+		if (filename == NO_SAVE_FILES) return;
+
+		filename += ".txt";
+
+		EvolutionSaver.LoadSimulationFromSaveFile(filename, creatureBuilder, evolution);
+	}
+
+	private void SetupDropDown() {
+
+		var filenames = EvolutionSaver.GetEvolutionSaveFilenames();
+
+		var saveFiles = new List<string>();
+
+		if (filenames.Count == 0) {
+			saveFiles.Add(NO_SAVE_FILES);
+		} else {
+			saveFilesExist = true;
+		}
+
+		foreach (var name in filenames) {
+			saveFiles.Add(name.Replace(".txt",""));
+		}
+
+		dropdown.ClearOptions();
+		dropdown.AddOptions(saveFiles);
+	}
+
+}
