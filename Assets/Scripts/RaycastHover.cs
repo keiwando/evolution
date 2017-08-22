946ca5c9c17440f78e48c9aba8083bf021e39802
Keiwan Donyagard <keiwan@gmx.de>
2017-08-22 16:05:46 +0200

    Fixed OnMouseOver not firing for overlapping colliders by replacing it with a RaycastHovering script

---
+++ Assets/Scripts/RaycastHover.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class RaycastHover : MonoBehaviour {
+
+	//private bool hovering = false;
+	private HashSet<Collider> hoverColliders = new HashSet<Collider>();
+
+	void Update () {
+
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		var hits = Physics.RaycastAll(ray);
+
+		if (hits.Length > 0) {
+
+			foreach (var hit in hits) {
+				SendOnHover(hit.collider);
+
+				hoverColliders.Add(hit.collider);
+			}
+
+			var hitSet = new HashSet<Collider>(hits.Select(hit => hit.collider));
+
+			foreach (var collider in hoverColliders.Except(hitSet)) {
+				SendOnHoverExit(collider);
+			}
+
+			hoverColliders = hitSet;
+		
+		} else if (hoverColliders.Count > 0) {
+			// Nothing hovered over -> exit on all
+			foreach (var collider in hoverColliders) {
+				SendOnHoverExit(collider);
+			}
+
+			hoverColliders.Clear();
+		}
+	}
+
+	private void SendOnHover(Collider collider) {
+		collider.SendMessage("OnHover", SendMessageOptions.DontRequireReceiver);
+	}
+
+	private void SendOnHoverExit(Collider collider) {
+		collider.SendMessage("OnHoverExit", SendMessageOptions.DontRequireReceiver);
+	}
+}
