02281064b53294290820ea3dcecf179325e2b09b
Keiwan Donyagard <keiwan@gmx.de>
2019-06-02 20:48:58 +0200

    Began Settings Menu redesign

---
+++ Assets/Scripts/View/ClickableTooltip.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Keiwando.Evolution.UI {
+
+    [RequireComponent(typeof(Button))]
+    public class ClickableTooltip: MonoBehaviour {
+
+        /// <summary>
+        /// The container which the tooltip gameobject needs to be parented
+        /// to, so that it renders after everything else inside of that 
+        /// container.
+        /// </summary>
+        [SerializeField] private Transform rootContainer;
+
+        [SerializeField] private GameObject tooltip;
+
+        private bool clickedThisFrame = false;  
+        private Transform previousParent;
+
+        void Start() {
+
+            if (tooltip == null) {
+                tooltip = transform.GetChild(0).gameObject;
+            }
+
+            var button = GetComponent<Button>();
+            button.onClick.AddListener(delegate () {
+                clickedThisFrame = true;
+                if (tooltip.activeSelf) {
+                    HideTooltip();
+                } else {
+                    ShowTooltip();
+                }
+            });
+
+            tooltip.gameObject.SetActive(false);
+            previousParent = tooltip.transform.parent;
+        }
+
+        void Update() {
+
+            // Hide the tooltip with a click
+            if (tooltip.activeSelf) {
+                if (!clickedThisFrame && Input.GetMouseButtonUp(0)) {
+                    HideTooltip();
+                } else {
+                    clickedThisFrame = false;
+                }
+            }
+        }
+
+        private void ShowTooltip() {
+            tooltip.transform.SetParent(rootContainer, true);
+            tooltip.transform.SetAsLastSibling();
+            tooltip.gameObject.SetActive(true);
+        }
+
+        private void HideTooltip() {
+            tooltip.SetActive(false);
+            tooltip.transform.SetParent(previousParent, true);
+        }
+    }
+}
---
+++ Assets/Scripts/View/SettingsViewController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Keiwando.Evolution.UI {
+
+    public class SettingsViewController: MonoBehaviour {
+        
+    }
+}
